819b036b586420ebf498899ed1214ee5a01b01ea
Darlington02 <darlingtonnnam@gmail.com>
2022-08-31 00:54:09 +0100

    deploy script for basic token

---
+++ contracts/BasicNft.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNFT is ERC721 {
+    string public constant TOKEN_URI = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    uint256 private tokenCounter;
+    constructor() ERC721("Cutie", "CUTE") {
+        tokenCounter = 0;
+    }
+
+    function mintNft() public returns(uint256) {
+        _safeMint(msg.sender, tokenCounter);
+        tokenCounter += 1;
+        return tokenCounter;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns(uint256) {
+        return tokenCounter;
+    }
+}
